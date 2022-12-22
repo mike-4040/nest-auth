7364dc3990dd771c8e8dc24fe0ad1f3c85500b2c
Mike Kravtsov <mike@index.supply>
2022-12-22 13:50:14 -0800

    guards

---
+++ src/iam/authentication/guards/access-token/access-token.guard.ts
@@ -0,0 +1,52 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from '../../../config/jwt.config';
+import { Request } from 'express';
+import { REQUEST_USER_KEY } from '../../../iam.constants';
+
+@Injectable()
+export class AccessTokenGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromRequest(request);
+    if (!token) {
+      throw new UnauthorizedException('Access token is missing');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        this.jwtConfiguration,
+      );
+      request[REQUEST_USER_KEY] = payload;
+    } catch (err) {
+      throw new UnauthorizedException('Invalid access token');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromRequest(request: Request) {
+    const authorizationHeader = request.headers.authorization;
+    if (!authorizationHeader) {
+      return null;
+    }
+    const [bearer, token] = authorizationHeader.split(' ');
+    if (bearer !== 'Bearer' || !token) {
+      return null;
+    }
+    return token;
+  }
+}
